50c7de49399d6729aa460bee97820c0531e947cb
awerni <andreas@wernitznig.com>
2024-03-05 14:45:23 +0100

    inital import

---
+++ fig3_EGFR_waterfall_selection/EGFR_waterfall.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+library(extrafont)
+library(XIFF)
+library(CLIFF)
+
+s <- getSettings()
+print(s$db)
+setDbOptions(s)
+
+cl_anno <- getCellLineAnno("human") |> 
+  filter(grepl("(lung|head|esopha)", tumortype))
+
+gene_anno <- getGeneAnno("human") |>
+  filter(grepl("^.{1,2}:", location))
+
+EGFR_anno <- gene_anno |> filter(symbol == "EGFR")
+
+expr_EGFR <- getCelllineDataGeneExpressionById(EGFR_anno$ensg, cl_anno$celllinename) |>
+  mutate(log2tpm = log2(tpm)) |>
+  left_join(cl_anno, by = "celllinename") |>  
+  reorderByScore(valueCol = "log2tpm")
+
+generateExpressionWaterfallPlot(expr_EGFR)
+ggsave("EGFR_expr_waterfall_3TT.pdf", width = 15, height = 5)
