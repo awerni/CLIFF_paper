ee1897267eae4ee90dd64798a5ba5e7104300de0
awerni <andreas@wernitznig.com>
2024-03-08 12:01:02 +0100

    modified filename

--- fig3_EGFR_waterfall_selection/EGFR_waterfall.R
+++ fig3_EGFR_waterfall_selection/EGFR_waterfall.R
@@ -7,7 +7,7 @@ s <- getSettings()
 print(s$db)
 setDbOptions(s)
 
-cl_anno <- getCellLineAnno("human") |> 
+cl_anno <- getCellLineAnno("human") |>
   filter(grepl("(lung|head|esopha)", tumortype))
 
 gene_anno <- getGeneAnno("human") |>
@@ -17,8 +17,8 @@ EGFR_anno <- gene_anno |> filter(symbol == "EGFR")
 
 expr_EGFR <- getCelllineDataGeneExpressionById(EGFR_anno$ensg, cl_anno$celllinename) |>
   mutate(log2tpm = log2(tpm)) |>
-  left_join(cl_anno, by = "celllinename") |>  
+  left_join(cl_anno, by = "celllinename") |>
   reorderByScore(valueCol = "log2tpm")
 
 generateExpressionWaterfallPlot(expr_EGFR)
-ggsave("EGFR_expr_waterfall_3TT.pdf", width = 15, height = 5)
+ggsave("fig3_EGFR_expr_waterfall_4TT.pdf", width = 15, height = 5)
